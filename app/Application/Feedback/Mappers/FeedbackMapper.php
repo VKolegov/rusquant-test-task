92454540adbc816ce4496f866480e0f6416ff6f2
Vladislav Kolegov <dev@vkolegov.ru>
2024-01-18 17:38:13 +0700

    [Application] Feedback mapper: from/to eloquent

--- app/Application/Feedback/Mappers/FeedbackMapper.php
+++ app/Application/Feedback/Mappers/FeedbackMapper.php
@@ -3,10 +3,12 @@
 namespace App\Application\Feedback\Mappers;
 
 use App\Domain\Feedback\Feedback;
-use App\Domain\Feedback\ValueObjects\FeedbackText;
 use App\Domain\Feedback\ValueObjects\AuthorName;
+use App\Domain\Feedback\ValueObjects\FeedbackText;
 use App\Domain\Shared\ValueObject\Email;
+use App\Infrastructure\Laravel\Models\FeedbackModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class FeedbackMapper
 {
@@ -19,4 +21,23 @@ class FeedbackMapper
         );
     }
 
+    public static function fromEloquent(FeedbackModel $eloquentModel): Feedback
+    {
+        return new Feedback(
+            authorName: new AuthorName($eloquentModel->author_name),
+            email: new Email($eloquentModel->email),
+            text: new FeedbackText($eloquentModel->text),
+            id: $eloquentModel->ulid
+        );
+    }
+
+    public static function toEloquent(Feedback $feedback): FeedbackModel
+    {
+        return new FeedbackModel([
+            'author_name' => $feedback->authorName->__toString(),
+            'email' => $feedback->email->__toString(),
+            'text' => $feedback->text->__toString(),
+            'created_at' => Carbon::parse($feedback->createdAt->raw()),
+        ]);
+    }
 }
